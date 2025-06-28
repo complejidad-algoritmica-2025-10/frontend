98302a4221496900f9da92761f33e3e294cabf19
L1LZ4Z <joaquinorozco2004@gmail.com>
2025-06-27 19:33:21 -0500

    feat: add route management from menu bar

--- src/graph/components/navbar-header.vue
+++ src/graph/components/navbar-header.vue
@@ -1,11 +1,26 @@
 <template>
-  <div class="card">
+  <div>
     <pv-menubar :model="items" class="custom-menubar">
+      <!-- Logo del menú -->
       <template #start>
         <div class="flex items-center gap-2">
           <img src="../../assets/logo.svg" alt="Logo" class="h-3 w-3" />
           <p class="font-bold text-lg vertical-align-middle" style="color: white;">Pantalla Justa</p>
         </div>
+      </template>
+
+      <!-- Slot personalizado para manejar routing con router-link -->
+      <template #item="{ item, props }">
+        <router-link v-if="item.to" v-slot="{ href, navigate, isActive }" :to="item.to" custom>
+          <a v-bind="props.action" :href="href" @click="navigate" :class="{ 'active-link': isActive }">
+            <span :class="item.icon" />
+            <span class="ml-2">{{ item.label }}</span>
+          </a>
+        </router-link>
+        <a v-else v-bind="props.action">
+          <span :class="item.icon" />
+          <span class="ml-2">{{ item.label }}</span>
+        </a>
       </template>
     </pv-menubar>
   </div>
@@ -18,7 +33,7 @@ import {InputText as PvInputText} from "primevue";
 interface MenuItem {
   label: string;
   icon?: string;
-  badge?: number;
+  to?: string;
   items?: MenuItem[];
 }
 
@@ -30,19 +45,23 @@ export default defineComponent({
       items: [
         {
           label: 'About',
-          icon: 'pi pi-venus'
+          icon: 'pi pi-venus',
+          to: '/about'
         },
         {
-          label: 'Colaborations',
-          icon: 'pi pi-users'
+          label: 'Collaborations',
+          icon: 'pi pi-users',
+          to: '/collaborations'
         },
         {
           label: 'Movie Staff',
-          icon: 'pi pi-ticket'
+          icon: 'pi pi-ticket',
+          to: '/movie-staff'
         },
         {
           label: 'Developers',
-          icon: 'pi pi-code'
+          icon: 'pi pi-code',
+          to: '/developers'
         }
       ] as MenuItem[]
     };
